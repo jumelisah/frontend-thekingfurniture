7babed80448dca9dd1203b94c03956b0729fd366
juumelisa <jumelisa09@gmail.com>
2022-05-24 03:24:56 +0700

    feat: env

--- helpers/http.js
+++ helpers/http.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const { NEXT_PUBLIC_BACKEND_URL } = process.env
 
 const http = (token) => {
+  console.log(NEXT_PUBLIC_BACKEND_URL)
+  console.log(process.env.NEXT_PUBLIC_FRONTEND_URL)
   const headers = {}
   if (token) {
     headers["Authorization"] = `Bearer ${token}`
--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    // NEXT_PUBLIC_BACKEND_URL: 'http://3.83.39.175:4000',
-    NEXT_PUBLIC_BACKEND_URL: 'http://localhost:5000',
-    // NEXT_PUBLIC_BACKEND_URL: 'http://3.83.39.175:4000'
-  },
   images: {
     // loader: "cloudinary",
     // path: "https://res.cloudinary.com",
